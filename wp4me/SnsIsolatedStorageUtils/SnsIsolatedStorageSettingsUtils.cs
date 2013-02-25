37430f4778bfc1b342d8e7e86432b57cf59f88f0
LudovicSmartnSoft <ludovic@smartnsoft.com>
2013-02-25 11:43:32 +0100

    Creation of the package SnsIsolatedStorageUtils

---
+++ wp4me/SnsIsolatedStorageUtils/SnsIsolatedStorageSettingsUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.IO.IsolatedStorage;
+
+namespace wp4me.SnsIsolatedStorageUtils
+{
+    /// <summary>
+    /// Class that provides functions to work with the IsolatedStorageSettings.
+    /// </summary>
+    public sealed class SnsIsolatedStorageSettingsUtils
+    {
+        /// <summary>
+        /// Method that saves a value into ApplicationSettings.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void SetApplicationSettings(string key, object value)
+        {
+            IsolatedStorageSettings.ApplicationSettings[key] = value;
+        }
+
+        /// <summary>
+        /// Method that returns the ApplicationSettings' value according to the key.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>value</returns>
+        public static object GetApplicationSettings(string key)
+        {
+            return IsolatedStorageSettings.ApplicationSettings[key];
+        }
+    }
+}
