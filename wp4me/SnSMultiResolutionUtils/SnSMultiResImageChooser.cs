1c0974e6c5312651cd267c1a813d8b3c734a2b76
LudovicSmartnSoft <ludovic@smartnsoft.com>
2013-02-25 11:42:44 +0100

    Creation of the package SnsMultiResolutionUtils

---
+++ wp4me/SnSMultiResolutionUtils/SnSMultiResImageChooser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace wp4me.SnSMultiResolutionUtils
+{
+    /// <summary>
+    /// Class that provides functions to work with the best resolution image according to the phone resolution.
+    /// </summary>
+    public sealed class SnSMultiResImageChooser
+    {
+        /// <summary>
+        /// Method that returns the best resolution image according to the phone resolution.
+        /// </summary>
+        /// <param name="imageName">Path of the image</param>
+        /// 
+        public Uri BestResolutionImage(string imageName)
+        {
+            var imageArray = imageName.Split('.');
+            var dictionaryImage = new Dictionary<string, string>();
+
+            dictionaryImage["name"] = imageArray[0];
+            dictionaryImage["extension"] = imageArray[1];
+
+            switch (ResolutionHelper.CurrentResolution)
+            {
+                case Resolutions.HD720P:
+                    return new Uri(dictionaryImage["name"] + "-720p." + dictionaryImage["extension"], UriKind.Relative);
+
+                case Resolutions.WXGA:
+                    return new Uri(dictionaryImage["name"] + "-wxga." + dictionaryImage["extension"], UriKind.Relative);
+
+                case Resolutions.WVGA:
+                    return new Uri(dictionaryImage["name"] + "-wvga." + dictionaryImage["extension"], UriKind.Relative);
+
+                default:
+                    throw new InvalidOperationException("Unknown resolution type");
+            }
+        }
+    }
+}
---
+++ wp4me/SnSMultiResolutionUtils/SnSResolutionHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+
+namespace wp4me.SnSMultiResolutionUtils
+{
+    public enum Resolutions { WVGA, WXGA, HD720P };
+
+    /// <summary>
+    /// Class that provides functions to work with the phone's resolution.
+    /// </summary>
+    public static class ResolutionHelper
+    {
+        private static bool IsWvga
+        {
+            get
+            {
+                return Application.Current.Host.Content.ScaleFactor == 100;
+            }
+        }
+
+        private static bool IsWxga
+        {
+            get
+            {
+                return Application.Current.Host.Content.ScaleFactor == 160;
+            }
+        }
+
+        private static bool Is720P
+        {
+            get
+            {
+                return Application.Current.Host.Content.ScaleFactor == 150;
+            }
+        }
+
+        public static Resolutions CurrentResolution
+        {
+            get
+            {
+                if (IsWvga) return Resolutions.WVGA;
+                if (IsWxga) return Resolutions.WXGA;
+                if (Is720P) return Resolutions.HD720P;
+                
+                throw new InvalidOperationException("Unknown resolution");
+            }
+        }
+    }
+}
