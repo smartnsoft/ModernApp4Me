1445030410c868dc2853b02110840bee0d9d2717
LudovicSmartnSoft <ludovic@smartnsoft.com>
2013-02-25 16:57:12 +0100

    Added the date.

--- wp4me/SnsDebugUtils/SnSDebugUtils.cs
+++ wp4me/SnsDebugUtils/SnSDebugUtils.cs
@@ -1,4 +1,6 @@
-﻿namespace wp4me.SnsDebugUtils
+﻿using System;
+
+namespace wp4me.SnsDebugUtils
 {
     /// <summary>
     /// Class that provides function to work debugs tools.
@@ -6,12 +8,12 @@
     public sealed class SnSDebugUtils
     {
         /// <summary>
-        /// Method that write text into the console.
+        /// Method that writes text into the console.
         /// </summary>
         /// <param name="text"></param>
         public static void ConsoleWriteLine(string text)
         {
-            System.Diagnostics.Debug.WriteLine(text);
+            System.Diagnostics.Debug.WriteLine(DateTime.Now.ToString("HH:mm:ss") + " " + text);
         }
     }
 }
