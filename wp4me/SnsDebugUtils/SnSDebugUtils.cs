e2c5651f959fed4b964ca4404ec74d26340178fd
LudovicSmartnSoft <ludovic@smartnsoft.com>
2013-02-25 11:44:04 +0100

    Creation of the package SnsDebugUtils.

---
+++ wp4me/SnsDebugUtils/SnSDebugUtils.cs
@@ -0,0 +1,17 @@
+﻿namespace wp4me.SnsDebugUtils
+{
+    /// <summary>
+    /// Class that provides function to work debugs tools.
+    /// </summary>
+    public sealed class SnSDebugUtils
+    {
+        /// <summary>
+        /// Method that write text into the console.
+        /// </summary>
+        /// <param name="text"></param>
+        public static void ConsoleWriteLine(string text)
+        {
+            System.Diagnostics.Debug.WriteLine(text);
+        }
+    }
+}
