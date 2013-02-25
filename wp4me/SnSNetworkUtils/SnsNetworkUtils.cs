745a6e47d795c12be38921fda62805e123bd80e4
LudovicSmartnSoft <ludovic@smartnsoft.com>
2013-02-25 11:43:06 +0100

    Creation of the package SnsNetworkUtils

---
+++ wp4me/SnSNetworkUtils/SnsNetworkUtils.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Phone.Net.NetworkInformation;
+using wp4me.SnsIsolatedStorageUtils;
+
+namespace wp4me.SnSNetworkUtils
+{
+    /// <summary>
+    /// Class that provides functions to work with network information.
+    /// </summary>
+    public sealed class SnsNetworkUtils
+    {
+        /// <summary>
+        /// Methods that indicates if the phone has a network connection (edge, 3G, wifi, etc.).
+        /// </summary>
+        /// <returns>true or false depending on the phone's network connection</returns>
+        public static bool HasNetworkConnection()
+        {
+            return NetworkInterface.NetworkInterfaceType == NetworkInterfaceType.None;
+        }
+    }
+}
