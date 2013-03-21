f8e31f7333d64056196c11bbca50c7165b7b9507
LudovicSmartnSoft <ludovic@smartnsoft.com>
2013-03-21 17:51:25 +0100

    Added 2 classes for the MVVM pattern and the perstistant data.

---
+++ wp4me/SnSModelUtils/SnSModelBase.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace wp4me.SnSModelUtils
+{
+    /// <summary>
+    /// Base class to implement a persistant model class.
+    /// </summary>
+    public class SnSModelBase : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        /*******************************************************/
+        /** PROPERTIES.
+        /*******************************************************/
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangingEventHandler PropertyChanging;
+
+
+        /*******************************************************/
+        /** METHODS.
+        /*******************************************************/
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void OnPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+    }
+}
---
+++ wp4me/SnSViewModelUtils/SnSViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace wp4me.SnSViewModelUtils
+{
+    /// <summary>
+    /// Base class to implement the MVVM pattern.
+    /// </summary>
+    public class SnSViewModelBase : INotifyPropertyChanged
+    {
+        /*******************************************************/
+        /** PROPERTIES.
+        /*******************************************************/
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        /*******************************************************/
+        /** METHODS.
+        /*******************************************************/
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
