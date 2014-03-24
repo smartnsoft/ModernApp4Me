ca1484364e2f0dc2abbccfd46ae88e1937264291
LudovicSmartnSoft <ludovic@smartnsoft.com>
2014-03-24 18:04:50 +0100

    FSS-28 Updated the core project to version 1.0.1

---
+++ ModernApp4Me/ModernApp4Me.Core/SnSApp/SnSI18N.cs
@@ -0,0 +1,19 @@
+﻿namespace ModernApp4Me.Core.SnSApp
+{
+
+    /// <summary>
+    /// Contains various attributes in order to have the default dialog boxes related to exceptions i18ned.
+    /// </summary>
+    /// <author>Ludovic ROLAND</author>
+    /// <since>2014.03.24</since>
+    public sealed class SnSI18N
+    {
+
+        public string DialogBoxErrorTitle { get; set; }
+
+        public string InhandledProblemHint { get; set; }
+
+        public string ConnectivityProblemHint { get; set; }
+
+    }
+}
