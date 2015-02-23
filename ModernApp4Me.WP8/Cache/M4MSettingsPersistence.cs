4957a4a40b719ec8e26aa9b49d138be659821d3d
ludovicroland <ludovic@smartnsoft.com>
2015-02-23 22:21:21 +0100

    FSS-76 #time 15m Updated the Settings persistence class.

--- ModernApp4Me.WP8/Cache/M4MSettingsPersistence.cs
+++ ModernApp4Me.WP8/Cache/M4MSettingsPersistence.cs
@@ -43,11 +43,21 @@ namespace ModernApp4Me.WP8.Cache
             }
         }
 
-        public bool AddSetting(string key, object value)
+        /// <summary>
+        /// Adds the business object corresponding to the provided parameter into the Settings by specifying the key.
+        /// </summary>
+        /// <param name="key">the key corresponding to the business object</param>
+        /// <param name="value">the business object</param>
+        /// <returns>true in case of success, false otherwise</returns>
+        public bool Add(string key, object value)
         {
             lock (InstanceLock)
             {
-                M4MModernLogger.Instance.Debug("Adding the value : '" + value.ToString() + "'");
+                if (M4MModernLogger.Instance.IsDebugEnabled() == true)
+                {
+                    M4MModernLogger.Instance.Debug("Adding an object with the key '" + key + "' into the Settings");
+                }
+
                 var isAdded = true;
 
                 try
@@ -56,79 +66,124 @@ namespace ModernApp4Me.WP8.Cache
                     settings.Add(key, value);
                     settings.Save();
                 }
-                catch (Exception)
+                catch (Exception exception)
                 {
+                    if (M4MModernLogger.Instance.IsErrorEnabled() == true)
+                    {
+                        M4MModernLogger.Instance.Error("An error occurs while adding an object with the key : '" + key + "' into the Settings", exception);
+                    }
+
                     isAdded = false;
                 }
 
                 return isAdded;
-            }            
+            }
         }
 
-        public bool UpdateSetting(string key, object value)
+        /// <summary>
+        /// Retrieves the business object stores into the Settings and corresponding to the specifying key.
+        /// </summary>
+        /// <param name="key">the key corresponding to the business object</param>
+        /// <returns>an <see cref="object"/> which can be null</returns>
+        public object Get(string key)
         {
-            var isUpdated = true;
-
-            try
+            lock (Instance)
             {
-                mutex.WaitOne();
+                if (M4MModernLogger.Instance.IsDebugEnabled() == true)
+                {
+                    M4MModernLogger.Instance.Debug("Getting the object with the key '" + key + "' from the Settings");
+                }
 
-                var settings = IsolatedStorageSettings.ApplicationSettings;
-                settings[key] = value;
-                settings.Save();
-            }
-            catch (Exception)
-            {
-                isUpdated = false;
-            }
-            finally
-            {
-                mutex.ReleaseMutex();
-            }
+                object returnValue = null;
 
-            return isUpdated;
+                try
+                {
+                    returnValue = IsolatedStorageSettings.ApplicationSettings[key];
+                }
+                catch (Exception exception)
+                {
+                    if (M4MModernLogger.Instance.IsErrorEnabled() == true)
+                    {
+                        M4MModernLogger.Instance.Error("An error occurs while reading the object with the key : '" + key + "' from the Settings", exception);
+                    }
+
+                    returnValue = null;
+                }
+
+                return returnValue;
+            }
         }
 
-        public object GetSetting(string key)
+        /// <summary>
+        /// Deletes the business object stores into the Settings and corresponding to the specifying key.
+        /// </summary>
+        /// <param name="key">the key corresponding to the business object</param>
+        /// <returns>true in case of success, false otherwise</returns>
+        public bool Remove(string key)
         {
-            object value = null;
+            lock (Instance)
+            {
+                if (M4MModernLogger.Instance.IsDebugEnabled() == true)
+                {
+                    M4MModernLogger.Instance.Debug("Removing the object with the key '" + key + "' from the Settings");
+                }
 
-            try
-            {
-                mutex.WaitOne();
-                value = IsolatedStorageSettings.ApplicationSettings[key];
-            }
-            catch (Exception)
-            {
-            }
-            finally
-            {
-                mutex.ReleaseMutex();
-            }
+                var isRemoved = true;
 
-            return value;
+                try
+                {
+                    IsolatedStorageSettings.ApplicationSettings.Remove(key);
+                }
+                catch (Exception exception)
+                {
+                    if (M4MModernLogger.Instance.IsErrorEnabled() == true)
+                    {
+                        M4MModernLogger.Instance.Error("An error occurs while removing the object with the key : '" + key + "' from the Settings", exception);
+                    }
+
+                    isRemoved = false;
+                }
+
+                return isRemoved;
+            }
         }
 
-        public bool RemoveSetting(string key)
+        /// <summary>
+        /// Updates the business object stores into the Settings and corresponding to the specifying key.
+        /// </summary>
+        /// <param name="key">the key corresponding to the business object</param>
+        /// <param name="value">the business object</param>
+        /// <returns>true in case of success, false otherwise</returns>
+        public bool Update(string key, object value)
         {
-            var isRemoved = true;
-
-            try
+            lock (Instance)
             {
-                mutex.WaitOne();
+                if (M4MModernLogger.Instance.IsDebugEnabled() == true)
+                {
+                    M4MModernLogger.Instance.Debug("Updating the object with the key '" + key + "' into the Settings");
+                }
 
-                IsolatedStorageSettings.ApplicationSettings.Remove(key);
-            }
-            catch (Exception)
-            {
-                isRemoved = false;
-            }
-            finally
-            {
-                mutex.ReleaseMutex();
-            }
+                var isUpdated = true;
 
-            return isRemoved;
+                try
+                {
+                    var settings = IsolatedStorageSettings.ApplicationSettings;
+                    settings[key] = value;
+                    settings.Save();
+                }
+                catch (Exception exception)
+                {
+                    if (M4MModernLogger.Instance.IsErrorEnabled() == true)
+                    {
+                        M4MModernLogger.Instance.Error("An error occurs while updating the object with the key : '" + key + "' intro the Settings", exception);
+                    }
+
+                    isUpdated = false;
+                }
+
+                return isUpdated;
+            }
         }
+        
     }
 }
