ad2828c51ceb860386247427520720fe2518ced2
ludovicroland <ludovic@smartnsoft.com>
2015-02-26 09:05:15 +0100

    FSS-61 #time 30m #resolve Added the licenses header

--- ModernApp4Me.WP8/Util/M4MResolutionImageChooser.cs
+++ ModernApp4Me.WP8/Util/M4MResolutionImageChooser.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿// Copyright (C) 2015 Smart&Soft SAS (http://www.smartnsoft.com/) and contributors
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// Contributors:
+//   Smart&Soft - initial API and implementation
+
+using System;
 using System.Text;
 using ModernApp4Me.WP8.Metadata;
 
