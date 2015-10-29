7c2582112ad8e4757430b02a44a569466ce94cb4
ludovicroland <ludovic@smartnsoft.com>
2015-10-29 17:39:59 +0100

    Updated the version name and code of the wp8 module.

--- ModernApp4Me.WP8/Properties/AssemblyInfo.cs
+++ ModernApp4Me.WP8/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Resources;
 //
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
-[assembly: AssemblyVersion("1.1.1.4")]
-[assembly: AssemblyFileVersion("1.1.1.4")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
 [assembly: NeutralResourcesLanguageAttribute("fr-FR")]
