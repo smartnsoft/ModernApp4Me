2c2c63260e9676887b55ab630c136f215c7e30f7
ludovicroland <ludovic@smartnsoft.com>
2015-12-10 11:07:53 +0100

    FSS-28 #time 15m Updated dependencies

--- ModernApp4Me.Core/WebServiceCache/M4MBaseBackedWSUriStringParser.cs
+++ ModernApp4Me.Core/WebServiceCache/M4MBaseBackedWSUriStringParser.cs
@@ -23,33 +23,33 @@ using RestSharp.Portable;
 namespace ModernApp4Me.Core.WebServiceCache
 {
 
-    /// <summary>
-    /// A basis class for managing cache data, making web service calls and parsing json response which uses RestSharp.Portable and JSON.NET third party libraries.
-    /// </summary>
-    /// 
-    /// <author>Ludovic ROLAND</author>
-    /// <since>2015.02.24</since>
-    public abstract class M4MBaseBackedWSUriStringParser<TResult, TParameter, TWebServiceCaller> where TWebServiceCaller : M4MBaseWebServiceCaller
+  /// <summary>
+  /// A basis class for managing cache data, making web service calls and parsing json response which uses RestSharp.Portable and JSON.NET third party libraries.
+  /// </summary>
+  /// 
+  /// <author>Ludovic ROLAND</author>
+  /// <since>2015.02.24</since>
+  public abstract class M4MBaseBackedWSUriStringParser<TResult, TParameter, TWebServiceCaller> where TWebServiceCaller : M4MBaseWebServiceCaller
+  {
+
+    public TWebServiceCaller WebServiceCaller { get; set; }
+
+    protected abstract RestRequest ComputeRestRequest(TParameter parameter);
+
+    protected abstract TResult Parse(byte[] response);
+
+    public abstract Task<TResult> GetRetentionValue(bool fromCache, DateTime expirationDelay, TParameter parameter);
+
+    public async Task<TResult> GetValue(TParameter parameter)
     {
-
-        public TWebServiceCaller WebServiceCaller { get; set; }
-
-        protected abstract RestRequest ComputeRestRequest(TParameter parameter);
-
-        protected abstract TResult Parse(byte[] response);
-
-        public abstract Task<TResult> GetRetentionValue(bool fromCache, DateTime expirationDelay, TParameter parameter);
-
-        public async Task<TResult> GetValue(TParameter parameter)
-        {
-            return await GetRetentionValue(false, DateTime.Now, parameter);
-        }
-
-        protected virtual TResult DeserializeObject(byte[] response)
-        {
-            return JsonConvert.DeserializeObject<TResult>(Encoding.UTF8.GetString(response, 0, response.Length));
-        }
-
+      return await GetRetentionValue(false, DateTime.Now, parameter);
     }
+
+    protected virtual TResult DeserializeObject(byte[] response)
+    {
+      return JsonConvert.DeserializeObject<TResult>(Encoding.UTF8.GetString(response, 0, response.Length));
+    }
+
+  }
 
 }
